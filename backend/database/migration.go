03d9f131b145b5e231a324b7e45ce457b8906273
rakuszz0 <ilahir66@gmail.com>
2025-05-14 14:01:55 +0800

    first commit

---
+++ backend/database/migration.go
@@ -0,0 +1,25 @@
+package database
+
+// import (
+// 	"zakat/models"
+// 	"zakat/pkg/postgres"
+// 	"fmt"
+// )
+
+// func RunMigration() {
+// 	err := postgres.DB.AutoMigrate(
+// 		&models.User{},
+// 		&models.Profile{},
+// 		&models.Product{},
+// 		&models.Cart{},
+// 		&models.Transaction{},
+// 		&models.ProductTransaction{},
+// 	)
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		panic("Migration Failed")
+// 	}
+
+// 	fmt.Println("Migration Success")
+// }
